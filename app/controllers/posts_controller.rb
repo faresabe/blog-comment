1a2359f455e1b3ed369864cc9557596859dd5c9a
faresabe <faresabenet9@gmail.com>
2025-05-20 23:11:27 +0300

    add new view and contoller for it

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,21 @@
 class PostsController < ApplicationController
+    def index
+        @post = Post.all
+    end
+     def new
+        @post = Post.new
+     end
+     def create 
+        @post = Post.new(Post_params)
+        if @post.save 
+            redirect_to @post
+        else
+            render "new"
+        end
+
+     end
+private
+def post_params
+    params.require(:Post).permit(:title, :content)
+end
 end
